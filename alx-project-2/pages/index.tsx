4eaaff5e5750032d04d567e392e2cf27023eaf40
Leon Gabriel <g73346920@gmail.com>
2025-01-07 14:04:36 +0300

    chored:modified

--- alx-project-2/pages/index.tsx
+++ alx-project-2/pages/index.tsx
@@ -11,12 +11,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {  
+export default function Home() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <h1>Welcome to our page</h1>
-   </div>
+    </div>
   );
 }
