5e31b919c62ee3c2d914e975b1581c1525447856
Leon Gabriel <g73346920@gmail.com>
2025-01-08 10:57:35 +0300

    Chored:Modified

--- alx-project-2/components/common/Card.tsx
+++ alx-project-2/components/common/Card.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Card:React.FC<CardProps> = ({title,content}) => {
   return (
     <div className="border rounded-lg shadow-md p-4 bg-white">
-      <h2 className="text-xl font-bold mb-2">{title}Maskoff</h2>
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
       <p className="text-gray-700">{content}</p>
     </div>
   )
