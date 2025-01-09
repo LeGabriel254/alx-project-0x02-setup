3117d95e3a723b9dddc5e4de7d5939b17b28ecbb
Leon Gabriel <g73346920@gmail.com>
2025-01-09 14:38:04 +0300

    Initisal commits

---
+++ alx-project-0x02/Components/layout/Header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div>
+ <li>
+      <Link href="/home" className="hover:text-gray-300">
+        Home
+      </Link>
+    </li>
+
+      <li>
+
+      <Link href="/about" className="hover:text-gray-300">
+        About
+      </Link>
+    </li>
+
+    </div>
+   
+  )
+}
+
+export default Header;
