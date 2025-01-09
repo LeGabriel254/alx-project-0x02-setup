b1c436e157bb72959def3cb81bc81a41e0c255e5
Leon Gabriel <g73346920@gmail.com>
2025-01-09 15:04:00 +0300

    modified

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,25 +1,35 @@
-import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Header: React.FC = () => {
-  return (
-    <div>
- <li>
-      <Link href="/home" className="hover:text-gray-300">
-        Home
-      </Link>
-    </li>
-
-      <li>
-
-      <Link href="/about" className="hover:text-gray-300">
-        About
-      </Link>
-    </li>
-
-    </div>
-   
-  )
-}
+    return (
+        <header className="bg-gray-800 p-4">
+            <nav className="container mx-auto">
+            <ul className="flex space-x-4">
+                <li>
+                <Link href="/home">
+                    <span className="text-white hover:text-gray-400">Home</span>
+                </Link>
+                </li>
+                <li>
+                <Link href="/about">
+                    <span className="text-white hover:text-gray-400">About</span>
+                </Link>
+                </li>
+                <li>
+                <Link href="/posts">
+                    <span className="text-white hover:text-gray-400">Posts</span>
+                </Link>
+                </li>
+                <li>
+                <Link href="/users">
+                    <span className="text-white hover:text-gray-400">Users</span>
+                </Link>
+                </li>
+            </ul>
+            </nav>
+        </header>
+    );
+};
 
 export default Header;
