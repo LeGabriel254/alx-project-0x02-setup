c56131728f8cb797eafcf0c82a160f57140383de
Leon Gabriel <g73346920@gmail.com>
2025-01-09 15:56:21 +0300

    Modified

---
+++ alx-project-0x02/pages/user.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { User } from "@/interfaces";
+
+export default function UsersPage() {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users");
+            const data = await response.json();
+            setUsers(data);
+        };
+        fetchUsers();
+    }, []);
+    return (
+        <div>
+            <Header />
+            <h1>Users Page</h1>
+            <h2 className="mt-4 text-xl font-bold">All Users</h2>
+            <div className="mt-4">
+                {users.map(user => (
+                    <UserCard key={user.id} name={user.name} email={user.email} address={user.address.street} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+// Fetch posts at build time
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+
+    return {
+        props: {
+            initialusers: data.slice(0, 10)
+        },
+    };
+}
